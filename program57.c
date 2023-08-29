4bb669b8d336d8680b41d95b98f8330577dc85f8
Shital Ganpat Rane <136183661+Shtal-1@users.noreply.github.com>
2023-08-29 16:36:46 +0545

    Update program57.c

--- program57.c
+++ program57.c
@@ -1,40 +1,4 @@
-// Input : 2    4
-// Output : 16  (2 * 2 * 2 * 2)
 
-/*
-    START
-        Accept one number as no1
-        Acccept another number as no2
-
-        Create one variable as Mult
-        Set the value 1 in the variable Mult
-
-        Create one countrer as cnt
-        Set the counter to 1
-
-        LOOP:
-        Itreate till the counter is less than or equal to no2
-            mult = Mult * no1
-            increment the counter by 1
-       Goto LOOP
-
-        Display the value of mult
-    END
-
-    no1 = 2
-    no2 = 4
-    mult = 1
-
-    mult = mult * no1
-    mult = mult * no1
-    mult = mult * no1
-    mult = mult * no1
-
-    for(cnt = 1; cnt <= no2; cnt++)
-    {
-        mult = mult * no1;
-    }
-*/
 
 #include<stdio.h>
 
